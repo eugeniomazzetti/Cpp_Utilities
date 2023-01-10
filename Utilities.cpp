e90818497ec1d525d2396f41ea8b2d080a58f73f
eugeniomazzetti <88039763+eugeniomazzetti@users.noreply.github.com>
2023-01-10 18:59:10 +0000

    Add files via upload

---
+++ Utilities.cpp
@@ -0,0 +1,29 @@
+//This source code file contains utilities function and classes
+//Creator: Eugenio Mazzetti
+
+//INCLUSION --------------------------------------------------------------------------------------------------------------
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+#include "Utilities.h" //Best practice
+
+// Floating Point Numbers Comparison -------------------------------------------------------------------------------------
+
+// This function compares two floating point numbers. Given their imprecise nature the function use "epsilon" to understand
+// if they are “close enough”. If they are then they are considered to be equal.
+
+bool approxEqualAbsRel(double x, double y, double absEpsilon = 1e-12, double relEpsilon = 1e-8)
+{
+	// (1) Check if floats are close in absolute value: Necessary when comparing numbers near zero
+	double diff{ std::abs(x - y) };
+
+	// (2) If they are close enough return true
+	if (diff <= absEpsilon)
+		return true;
+
+	// (3) Otherwise use Knuth's algo that considers numbers relative magnitude
+	return (diff <= (std::max(std::abs(x), std::abs(y)) * relEpsilon));
+
+}
+
---
+++ Utilities.h
@@ -0,0 +1,11 @@
+// Header Guard ----------------------------------------------------------------------------------------------------------
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+// Forward Declarations --------------------------------------------------------------------------------------------------
+
+// Floating Point Numbers Comparison 
+bool approxEqualAbsRel(double x, double y, double absEpsilon = 1e-12, double relEpsilon = 1e-8);
+
+
+#endif // !UTILITIES_H
